ee8f7f6ca917ac9d47056c536cbc4c539f1d2353
Nitheshnithu18 <nithesh09052001@gmail.com>
2025-09-15 12:36:54 +0530

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => (
+  <nav className="mb-6 flex justify-center space-x-4 p-4 rounded shadow-md">
+    <Link to="/" className="text-black-600 hover:underline">
+      Feedback Form
+    </Link>
+    <Link to="/dashboard" className="text-black-600 hover:underline">
+      Dashboard
+    </Link>
+  </nav>
+);
+
+export default Navbar;
